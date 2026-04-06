ece9ce3c393dc9e9293345918f3915715a3c93f7
abdullah ahmad <abdoehsan69@gmail.com>
2026-04-06 23:40:43 +0300

    Update home.tsx to load images from public/images directory

---
+++ home.tsx
@@ -0,0 +1,17 @@
+// home.tsx
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Our Automobile Services</h1>
+            <img src="/images/car1.jpg" alt="Car 1" />
+            <img src="/images/car2.jpg" alt="Car 2" />
+            <img src="/images/car3.jpg" alt="Car 3" />
+            <p>We provide the best services for your vehicle.</p>
+        </div>
+    );
+};
+
+export default Home;
